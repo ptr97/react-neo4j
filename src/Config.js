ddb9885b853756d64ba6ccf9cebe7c1ef0d31ca3
Piotr Wos <ptr.wos@gmail.com>
2019-12-10 22:19:43 +0100

    API uri from config

--- src/Config.js
+++ src/Config.js
@@ -3,5 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  API_URI: "http://68.183.145.245:3000/neo4j-graphql" || "http://localhost:5000/neo4j-graphql"
+  API_URI: process.env.API_URI || "http://localhost:5000/neo4j-graphql"
 };
